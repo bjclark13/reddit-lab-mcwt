847d8a9937c907f26142661cacb4fd3488afcef7
BJ Clark <bjclark13@outlook.com>
2021-01-20 21:15:15 -0500

    Add full solution

--- src/app/reddit-api.service.ts
+++ src/app/reddit-api.service.ts
@@ -2,17 +2,17 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
 interface RedditPost {
-  title: string;
-  link: string;
-  image: string;
-}
+  title: string; // title of post
+  link: string; // outbound link to the post on reddit
+  image: string; // thumbnail
+};
 
 @Injectable({
   providedIn: "root",
 })
 export class RedditApiService {
   // inject HTTP Client here
-  // posts: RedditPost[] = [];
+  posts: RedditPost[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -20,9 +20,20 @@ export class RedditApiService {
     const url = "https://www.reddit.com/r/aww/.json";
     this.http.get(url).subscribe(
       (resp: any) => {
-        console.log(resp);
-
         const posts = resp.data.children;
+
+        for (let post of posts) {
+          const redditPost : RedditPost = {
+            title: post.data.title,
+            link: "https://reddit.com" + post.data.permalink,
+            image: post.data.thumbnail
+          };
+
+          this.posts.push(redditPost);
+
+        }
+
+        console.log(this.posts);
   
       }, // this is what happens on success
       (error) => {
